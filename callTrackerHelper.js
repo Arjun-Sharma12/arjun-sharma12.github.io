ffa97a507703dbe21165650aee1d9107de35e3d5
ArjunProgramming <arjunsharmalegos@gmail.com>
2026-08-14 16:51:24 -0700

    Updated Icon

--- callTrackerHelper.js
+++ callTrackerHelper.js
@@ -2,7 +2,7 @@ Game.Win('Third-party');
 if(CallTrackerHelper === undefined) var CallTrackerHelper = {};
 if(typeof CCSE == 'undefined') Game.LoadMod('https://klattmose.github.io/CookieClicker/' + (0 ? 'Beta/' : '') + 'CCSE.js');
 CallTrackerHelper.name = 'Call Tracker Helper';
-CallTrackerHelper.pic = 'https://github.com/kyron377333333/cookie-clicker-source-code-/blob/main/img/alchemylab.png?raw=true';
+CallTrackerHelper.pic = 'https://arjun-sharma12.github.io/CookieClicker/img/alchemylab.png';
 CallTrackerHelper.version = '1.0';
 CallTrackerHelper.GameVersion = '2.058';
 
